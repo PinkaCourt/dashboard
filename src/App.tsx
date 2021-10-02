819ec4b58b68400d7b4c617238e568c902c10120
Polina <ekortika@gmail.com>
2021-10-02 22:33:19 +0300

    добавляем роут на главную

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import Auth from "pages/Auth";
+import SignUp from "pages/SignUp";
+import Dashboard from "pages/Dashboard";
+import Profile from "pages/Profile";
+import { store } from "store/store";
+
+export const routes = {
+  auth: {
+    path: "/",
+    exact: true,
+    component: Auth,
+  },
+  signUp: {
+    path: "/signUp",
+    exact: false,
+    component: SignUp,
+  },
+  dashboards: {
+    path: "/dashboard",
+    exact: false,
+    component: Dashboard,
+  },
+  profile: {
+    path: "/profile",
+    exact: false,
+    component: Profile,
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          {Object.values(routes).map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
